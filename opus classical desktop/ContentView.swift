99eeeccef9a5f6d463e9c55c36c98e010c8207b8
Denis Rodionov <nicholas.hawkwood@gmail.com>
2023-08-06 20:42:39 +0300

    Initial Commit

---
+++ opus classical desktop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  opus classical desktop
+//
+//  Created by Denis Rodionov on 6.8.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ opus classical desktop/opus_classical_desktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  opus_classical_desktopApp.swift
+//  opus classical desktop
+//
+//  Created by Denis Rodionov on 6.8.2023.
+//
+
+import SwiftUI
+
+@main
+struct opus_classical_desktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
